61c5b666d84506a4f9bec1fbfe4998e0d3ed2886
Hugo Ferreira <hugogferreira7@gmail.com>
2022-02-19 20:53:58 +0000

    Binary
    Added binary conversion

--- Text.h
+++ Text.h
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <sstream>
 #include <cctype>
+#include <bits/stdc++.h>
 using namespace std;
 
 class inputOutputText
@@ -16,6 +17,7 @@ public:
     string ironicText();
     string capitalLetter();
     string lowerLetter();
+    string stringToBinary();
     void showResults();
 };
 
@@ -30,13 +32,16 @@ void inputOutputText::receiveInput()
     output[0] = ironicText();
     output[1] = capitalLetter();
     output[2] = lowerLetter();
+    output[3] = stringToBinary();
     return;
 }
 
 string inputOutputText::ironicText()
 {
+    int n = text.length();
     text[0] = tolower(text[0]);
-    for(int i=1; i<text.length(); i++)
+
+    for(int i=1; i<n; i++)
     {
         if(islower(text[i-1]))
             text[i] = toupper(text[i]);
@@ -48,16 +53,39 @@ string inputOutputText::ironicText()
 
 string inputOutputText::capitalLetter()
 {
-    for(int i=0; i<text.length(); i++)
+    int n = text.length();
+
+    for(int i=0; i<n; i++)
         text[i] = toupper(text[i]);
     return text;
 }
 
 string inputOutputText::lowerLetter()
 {
-    for(int i=0; i<text.length(); i++)
+    int n = text.length();
+
+    for(int i=0; i<n; i++)
         text[i] = tolower(text[i]);
     return text;
+}
+
+string inputOutputText::stringToBinary()
+{
+    int n = text.length();
+    string bin = "";
+
+    for(int i=0; i<=n; i++)
+    {
+        int val = int(text[i]);
+        while(val > 0)
+        {
+            (val % 2) ? bin.push_back('1'):
+                        bin.push_back('0');
+            val /= 2;
+        }
+        reverse(bin.begin(), bin.end());
+    }
+    return bin;
 }
 
 void inputOutputText::showResults()
@@ -67,5 +95,7 @@ void inputOutputText::showResults()
     cout << "--------------";
     cout << "\n\nFull capital letter\n\n" << output[1] << endl << endl;
     cout << "--------------";
-    cout << "\n\nFull lower letter\n\n" << output[2] << endl;
+    cout << "\n\nFull lower letter\n\n" << output[2] << endl << endl;
+    cout << "--------------";
+    cout << "\n\nBinary\n\n" << output[3] << endl << endl;
 }
